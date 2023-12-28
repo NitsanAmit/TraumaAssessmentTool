0a1721e7ddd9b0f6afbf9f3234d1d27c6af3cc59
Nitsan Amit <nitsan.amit@riseup.co.il>
2023-12-28 14:11:22 +0200

    Add style classes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import './components/styles/spacings.css';
+import './components/styles/layout.css';
 import { App } from './components/App';
 
 const root = ReactDOM.createRoot(
