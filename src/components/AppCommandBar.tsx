415b2cb0e99195cb4696939e5d8e9289545e9194
Nitsan Amit <nitsan.amit@riseup.co.il>
2024-01-06 20:57:53 +0200

    Fix .env setup

--- src/components/AppCommandBar.tsx
+++ src/components/AppCommandBar.tsx
@@ -1,8 +1,8 @@
-import { ApplicationStateStore } from '../store/ApplicationStateStore';
+import { APPLICATION_STEP, ApplicationStateStore } from '../store/ApplicationStateStore';
 import { useMemo } from 'react';
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components';
-import { ChevronLeft24Regular } from '@fluentui/react-icons';
+import { ChevronLeft24Regular, ArrowHookUpLeft24Regular, ArrowBounce24Regular } from '@fluentui/react-icons';
 import { Button } from '@fluentui/react-components';
 import { useSearchParams } from 'react-router-dom';
 
@@ -15,11 +15,19 @@ export const AppCommandBar: React.FC<AppCommandBarProps> = observer(({ appStateS
     <CommandBar>
       <StepDisplayName>{appStateStore.stepDisplayName}</StepDisplayName>
       <div>
-        <Button appearance="subtle" iconPosition="after" size="small" icon={<ChevronLeft24Regular/>}
-                onClick={() => appStateStore.back()}>חזרה</Button>
         {
-          debugMode &&
+          appStateStore.step !== APPLICATION_STEP.SUMMARY &&
           <Button appearance="subtle" iconPosition="after" size="small" icon={<ChevronLeft24Regular/>}
+                  onClick={() => appStateStore.back()}>חזרה</Button>
+        }
+        {
+          appStateStore.step === APPLICATION_STEP.SUMMARY &&
+          <Button appearance="subtle" iconPosition="after" size="small" icon={<ArrowHookUpLeft24Regular/>}
+                  onClick={() => window.location.reload()}>התחלה מחדש</Button>
+        }
+        {
+          debugMode && appStateStore.step !== APPLICATION_STEP.SUMMARY &&
+          <Button appearance="subtle" iconPosition="after" size="small" icon={<ArrowBounce24Regular/>}
                   onClick={() => appStateStore.skip()}>דלג</Button>
         }
       </div>
