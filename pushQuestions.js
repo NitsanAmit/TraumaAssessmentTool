b7c661c255783f27eb7acf254a23a69dd02edcca
Nitsan Amit <nitsan.amit@riseup.co.il>
2024-02-08 18:35:55 +0200

    Fix merge workflow

--- pushQuestions.js
+++ pushQuestions.js
@@ -2,19 +2,13 @@ const { getApps, initializeApp, deleteApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 const { doc, setDoc } = require('firebase/firestore');
 const questionnaires = require('./src/data/questions.json');
-const dotenv = require('dotenv');
-dotenv.config();
 
-if (!process.env.REACT_APP_FIREBASE_API_KEY) {
-  console.error('REACT_APP_FIREBASE_API_KEY is not set.');
-  process.exit(1);
-}
 initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  appId: process.env.FIREBASE_APP_ID,
 });
 
 const firestore = getFirestore(getApps()[0]);
