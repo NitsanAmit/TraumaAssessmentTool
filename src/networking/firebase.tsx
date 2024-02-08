104a2f2b927a1a2ea9091611f9701b2c8362046a
Nitsan Amit <nitsan.amit@riseup.co.il>
2024-02-08 19:27:14 +0200

    Fix workflow?

--- src/networking/firebase.tsx
+++ src/networking/firebase.tsx
@@ -4,16 +4,7 @@ import { createContext, useEffect, useState } from 'react';
 export async function initializeFirebase(): Promise<FirebaseApp | undefined> {
   try {
     if (!getApps().length) {
-      const devEnv = process.env.NODE_ENV === 'development';
-      return initializeApp(devEnv ? {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_FIREBASE_APP_ID,
-        measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-      } : {
+      return initializeApp({
         apiKey: process.env.FIREBASE_API_KEY,
         authDomain: process.env.FIREBASE_AUTH_DOMAIN,
         projectId: process.env.FIREBASE_PROJECT_ID,
