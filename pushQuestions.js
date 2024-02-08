4b8037e7eefa5ced0eb3d6ee56758344b4ff01cc
Nitsan Amit <nitsan.amit@riseup.co.il>
2024-02-08 19:35:49 +0200

    Attempt to pass env vars to deployment

--- pushQuestions.js
+++ pushQuestions.js
@@ -4,11 +4,11 @@ const { doc, setDoc } = require('firebase/firestore');
 const questionnaires = require('./src/data/questions.json');
 
 initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const firestore = getFirestore(getApps()[0]);
