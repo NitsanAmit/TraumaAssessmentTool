6f35502bd1d977f17e8b2923dce43cf4574e1adf
Nitsan Amit <nitsan.amit@riseup.co.il>
2024-02-08 19:11:46 +0200

    Fix deployment env vars

--- src/networking/firebase.tsx
+++ src/networking/firebase.tsx
@@ -4,7 +4,8 @@ import { createContext, useEffect, useState } from 'react';
 export async function initializeFirebase(): Promise<FirebaseApp | undefined> {
   try {
     if (!getApps().length) {
-      return initializeApp({
+      const devEnv = process.env.NODE_ENV === 'development';
+      return initializeApp(devEnv ? {
         apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
         authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
         projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -12,6 +13,14 @@ export async function initializeFirebase(): Promise<FirebaseApp | undefined> {
         messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.REACT_APP_FIREBASE_APP_ID,
         measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+      } : {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
       });
     }
   } catch (error) {
